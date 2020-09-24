b9d9d1cf558925a9b029a89c2d8ac674a7e58d38
milan.zivanov <milan.zivanov@gmail.com>
2020-09-24 16:00:51 +0200

    Lesson 102. Get City API Call

--- weatherApp/scripts/forecast.js
+++ weatherApp/scripts/forecast.js
@@ -1,1 +1,17 @@
 const key = "s5ydbetqyHKbdYoazKPynMArw35YeQyP";
+
+const getCity = async (city) => {
+
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+}
+
+getCity("novi sad")
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
