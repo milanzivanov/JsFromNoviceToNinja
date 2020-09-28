b2663ccc3e1ede2c3b3f54d99d3fcf2be1f46cc7
milan.zivanov <milan.zivanov@gmail.com>
2020-09-28 12:11:17 +0200

    Lesson 70. Intervals & Animating the Score

--- quiz/app.js
+++ quiz/app.js
@@ -20,7 +20,19 @@ form.addEventListener("submit", e => {
     // show result on page
     // console.log(score);
     scrollTo(0, 0);
-    result.querySelector("span").textContent = `${score}%`;
     result.classList.remove("d-none");
+    
+    // score animation
+    let output = 0;
+    const timer = setInterval(() => {
+
+        result.querySelector("span").textContent = `${output}%`;
+        if(output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+        
+    }, 10);
 
 });
