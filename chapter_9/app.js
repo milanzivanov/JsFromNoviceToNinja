2e105bc3ac487a2e1362b336329bb42c29494f99
milan.zivanov <milan.zivanov@gmail.com>
2020-09-29 11:44:33 +0200

    Lesson 73. Reduce Method

--- chapter_9/app.js
+++ chapter_9/app.js
@@ -1,21 +1,45 @@
+// reduce method
 // 1
-const scores = [10, 30, 15, 55, 40, 5];
+// const scores = [20, 10, 37, 15, 55, 83, 90];
 
-const filterdScore = scores.filter( (score) => {
-    return score > 30;
-});
+// const result = scores.reduce((acc, curr) => {
 
-console.log(filterdScore);
+//     if(curr > 50) {
+//         acc++;
+//     }
+//     return acc;
+    
+// }, 0);
 
-// this method is nondestructive
-// console.log(scores);
+// console.log(result);
 
 // 2
-const users = [
-    {name: 'milan', premium: true},
-    {name: 'ivan', premium: true},
-    {name: 'vanja', premium: false}
-];
+// const scores = [
+//     {player: "milan", score: 50},
+//     {player: "miki", score: 30},
+//     {player: "milan", score: 70},
+//     {player: "milica", score: 60}
+// ];
 
-const premiumUser = users.filter( user => user.premium );
-console.log(premiumUser);
+// const total = scores.reduce((acc, curr) => {
+
+//     if (curr.player === "milan") {
+//         acc += curr.score;
+//     }
+//     return acc;
+
+// }, 0);
+
+// console.log(total);
+
+// mdn 
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
+
+// 5 + 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer, 5));
+// expected output: 15
