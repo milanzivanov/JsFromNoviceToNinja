d444256b827d0589ca1530ffaac136a17933555a
milan.zivanov <milan.zivanov@gmail.com>
2020-10-01 12:57:58 +0200

    Lesson 92. JSON Data

--- chapter_12/app.js
+++ chapter_12/app.js
@@ -5,13 +5,14 @@ const getTodos = (callback) => {
     request.addEventListener('readystatechange', () => {
         // console.log(request, request.readyState);
         if(request.readyState === 4 && request.status === 200) {
-            callback(undefined, request.responseText);
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
         } else if(request.readyState === 4) {
             callback('culd not fetch data', undefined);        
         }
     });
     
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET', './todos.json');
     request.send();
 
 }
