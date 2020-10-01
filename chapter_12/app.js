176c989a1daf762c1f8bb991d88c3f6f73d1788a
milan.zivanov <milan.zivanov@gmail.com>
2020-10-01 12:48:53 +0200

    Lesson 91. Callback Functions

--- chapter_12/app.js
+++ chapter_12/app.js
@@ -1,13 +1,28 @@
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
 
-request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-    if(request.readyState === 4 && request.status === 200) {
-        console.log(request, request.responseText);
-    } else if(request.readyState === 4) {
-        console.log('could not fetch the data');
+    const request = new XMLHttpRequest();
+    
+    request.addEventListener('readystatechange', () => {
+        // console.log(request, request.readyState);
+        if(request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText);
+        } else if(request.readyState === 4) {
+            callback('culd not fetch data', undefined);        
+        }
+    });
+    
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+
+}
+
+// error first convention
+getTodos((err, data) => {
+    console.log("callback fired");
+    // console.log(err, data);
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(data);
     }
 });
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
