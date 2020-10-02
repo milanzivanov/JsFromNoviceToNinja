608bc64a4cc31b808f4aa7e83569b1a72ffb7bb3
milan.zivanov <milan.zivanov@gmail.com>
2020-10-02 13:42:32 +0200

    Lesson 94. Promise Basics

--- chapter_12/app.js
+++ chapter_12/app.js
@@ -1,34 +1,57 @@
-const getTodos = (resource, callback) => {
+const getTodos = (resource) => {
 
-    const request = new XMLHttpRequest();
-    
-    request.addEventListener('readystatechange', () => {
-        // console.log(request, request.readyState);
-        if(request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if(request.readyState === 4) {
-            callback('culd not fetch data', undefined);        
-        }
-    });
-    
-    request.open('GET', resource);
-    request.send();
-
-}
-
-// error first convention
-getTodos('./todos/milan.json', (err, data) => {
-    console.log(data);
-
-    getTodos('./todos/mica.json', (err, data) => {
-        console.log(data);
-
-        getTodos('./todos/milanko.json', (err, data) => {
-            console.log(data);
+    return new Promise((resolve, reject) => {
+        
+        const request = new XMLHttpRequest();
+        
+        request.addEventListener('readystatechange', () => {
+            // console.log(request, request.readyState);
+            if(request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if(request.readyState === 4) {
+                reject('error getting resource');
+            }
         });
         
+        request.open('GET', resource);
+        request.send();
+
     });
 
+};
 
-});
+getTodos('./todos/milan.json').then((data) => {
+    console.log('Promise resolved:', data);
+}).catch(err => {
+    console.log(err);
+})
+
+
+
+
+// promise example
+// const getSomething = () => {
+
+//     return new Promise((resolve, reject) => {
+//         // fetch something
+//         // resolve('resolve some data');
+//         reject('some error');
+//     });
+
+// }
+
+// one way
+// getSomething().then((data) => {
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// });
+
+// shorter way
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
