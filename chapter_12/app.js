6a0dfb1433a21c4e9bbe599cf989069b0d268f04
milan.zivanov <milan.zivanov@gmail.com>
2020-10-03 16:33:07 +0200

    Lesson 97. Async & Await

--- chapter_12/app.js
+++ chapter_12/app.js
@@ -4,13 +4,19 @@
 const getTodos = async () => {
 
     const response = await fetch('./todos/milanko.json');
+
+    if(response.status !== 200) {
+        throw new Error('cannot fetch the data');
+    }
+
     const data = await response.json();
     return data;
 
 };
 
 getTodos()
-    .then(data => console.log('resolved', data));
+    .then(data => console.log('resolved', data))
+    .catch(err => console.log('rejected:', err.message));
 
 // my example from https://jsonplaceholder.typicode.com/
 // fetch('https://jsonplaceholder.typicode.com/todos/2')
