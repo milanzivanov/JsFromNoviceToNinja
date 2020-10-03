e587248b5e4cd5bc81938bd8451582300bb24879
milan.zivanov <milan.zivanov@gmail.com>
2020-10-03 14:04:45 +0200

    Lesson 96. The Fetch API

--- chapter_12/app.js
+++ chapter_12/app.js
@@ -1,63 +1,9 @@
-const getTodos = (resource) => {
-
-    return new Promise((resolve, reject) => {
-        
-        const request = new XMLHttpRequest();
-        
-        request.addEventListener('readystatechange', () => {
-            // console.log(request, request.readyState);
-            if(request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-            } else if(request.readyState === 4) {
-                reject('error getting resource');
-            }
-        });
-        
-        request.open('GET', resource);
-        request.send();
-
-    });
-
-};
-
-getTodos('./todos/milan.json').then((data) => {
-    console.log('Promise resolved milan:', data);
-    return getTodos('./todos/mica.json');
+// fetch api
+fetch('./todos/milan.json').then((response) => {
+    console.log('Resolved', response);
+    return response.json();
 }).then(data => {
-    console.log('Promise resolved mica:', data);
-    return getTodos('./todos/milanko.json');
-}).then(data => {
-    console.log('Promise resolved milanko:',data);
-}).catch(err => {
-    console.log('Promise rejected', err);
-})
-
-
-
-
-// promise example
-// const getSomething = () => {
-
-//     return new Promise((resolve, reject) => {
-//         // fetch something
-//         // resolve('resolve some data');
-//         reject('some error');
-//     });
-
-// }
-
-// one way
-// getSomething().then((data) => {
-//     console.log(data);
-// }, (err) => {
-//     console.log(err);
-// });
-
-// shorter way
-// getSomething().then(data => {
-//     console.log(data);
-// }).catch(err => {
-//     console.log(err);
-// });
-
+    console.log(data);
+}).catch((err) => {
+    console.log('Rejected', err);
+});
