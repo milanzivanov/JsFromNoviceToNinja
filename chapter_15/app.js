0e251ad7b5527780f93554cb5404255fa3d2f671
milan.zivanov <milan.zivanov@gmail.com>
2020-10-05 12:55:53 +0200

    Lesson 119. Class Methods & Method Chaining

--- chapter_15/app.js
+++ chapter_15/app.js
@@ -3,12 +3,30 @@ class User {
         // set up properties
         this.username = username;
         this.email = email;
+        this.score = 0;
+    }
+    login() {
+        console.log(`${this.username} just logged in`);
+        return this;
+    }
+    logout() {
+        console.log(`${this.username} just logout`);
+        return this;
+    }
+    incScore() {
+        this.score++; // or this.score += 1;
+        console.log(`${this.username} has a score of ${this.score}`);
+        return this;
     }
 }
 
 const userOne = new User('milan', 'milan.zivanov@gmail.com');
 const userTwo = new User('ivan', 'ivan.zivanov@gmail.com');
+
 console.log(userOne, userTwo);
+userOne.login();
+userOne.logout();
+userOne.incScore().incScore().incScore().logout();
 
 // The 'new' keyword
 // 1 - it creates a new empty object {}
