4bda56f8b1937819cfdc62eca72d3d02d31310fa
milan.zivanov <milan.zivanov@gmail.com>
2020-10-06 11:14:33 +0200

    Lesson 120. Class Inheritance (subclasses)

--- chapter_15/app.js
+++ chapter_15/app.js
@@ -20,13 +20,28 @@ class User {
     }
 }
 
+class Admin extends User {
+    deleteUser(user) {
+        console.log("test", user);
+        // users = users.filter(u => u.username !== user.username);
+        users = users.filter(u => console.log("this", u));
+    }
+}
+
 const userOne = new User('milan', 'milan.zivanov@gmail.com');
 const userTwo = new User('ivan', 'ivan.zivanov@gmail.com');
+const userThree = new Admin('ivanka', 'ivan.ka@gmail.com');
 
-console.log(userOne, userTwo);
+console.log(userOne, userTwo, userThree);
 userOne.login();
 userOne.logout();
 userOne.incScore().incScore().incScore().logout();
+
+let users = [userOne, userTwo, userThree];
+console.log(users);
+
+userThree.deleteUser(userTwo);
+console.log(users);
 
 // The 'new' keyword
 // 1 - it creates a new empty object {}
