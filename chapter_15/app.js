2daf591f80091d032a3c70e4e8dc58106c9d9e26
milan.zivanov <milan.zivanov@gmail.com>
2020-12-04 12:04:55 +0100

    Lesson 122. Constructors (under the hood)

--- chapter_15/app.js
+++ chapter_15/app.js
@@ -1,45 +1,27 @@
-class User {
-    constructor(username, email) {
-        // set up properties
-        this.username = username;
-        this.email = email;
-        this.score = 0;
-    }
-    login() {
-        console.log(`${this.username} just logged in`);
-        return this;
-    }
-    logout() {
-        console.log(`${this.username} just logout`);
-        return this;
-    }
-    incScore() {
-        this.score++; // or this.score += 1;
-        console.log(`${this.username} has a score of ${this.score}`);
-        return this;
+// constructor function
+
+function User(username, email) {
+    this.username = username;
+    this.email = email;
+    this.login = function() {
+        console.log(`${this.username} has logged!!!`);
     }
 }
 
-class Admin extends User {
-
-    constructor(username, email, title) {
-        super(username, email);
-        this.title = title;
-    }
-
-    deleteUser(user) {
-        // console.log("test", user);
-        users = users.filter(u => u.username !== user.username);
-        // users = users.filter(u => console.log("this", u));
-    }
-}
+// class User {
+//     constructor(username, email) {
+//         // set up properties
+//         this.username = username;
+//         this.email = email;
+//     }
+// }
 
 const userOne = new User('milan', 'milan.zivanov@gmail.com');
 const userTwo = new User('ivan', 'ivan.zivanov@gmail.com');
-const userThree = new Admin('ivanka', 'ivan.ka@gmail.com', 'black-belt-ninja');
 
-console.log(userThree);
-
+console.log(userOne, userTwo);
+userOne.login();
+userTwo.login();
 
 // The 'new' keyword
 // 1 - it creates a new empty object {}
